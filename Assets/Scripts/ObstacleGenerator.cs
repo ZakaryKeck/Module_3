c82fe706a4fd8234fbe73520f1a2b4de11e01dd7
ZakaryKeck <zakary.keck@huskers.unl.edu>
2020-02-05 16:47:09 -0600

    Random cubes move toward

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    
+    private float obstacleSpawnLocation = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("DoSpawnObstacle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        int obstacleIndex = Random.Range(0, 3);
+        GameObject obstacle = Instantiate(obstaclePrefabs[obstacleIndex]) as GameObject;
+        obstacle.transform.SetParent(transform);
+        obstacle.transform.position = Vector3.forward * obstacleSpawnLocation + new Vector3(0,1,0);
+    }
+
+    IEnumerator DoSpawnObstacle()
+    {
+        while (true)
+        {
+            SpawnObstacle();
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
